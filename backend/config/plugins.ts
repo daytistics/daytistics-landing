c977a71972c5d99b95727631876f888d33d7ea5a
Leo Gall <gall.dev@proton.me>
2024-11-04 17:26:43 +0100

    fix deploy #19

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,9 +1,9 @@
-const crypto = require('crypto');
+import * as crypto from "crypto";
 
 module.exports = ({ env }) => ({
-  'users-permissions': {
+  "users-permissions": {
     config: {
-      jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
+      jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
     },
   },
 });
